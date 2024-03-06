d0a4c86da277dec0b599f0352f034ae55d037490
PPC <ppc@Nak-3.local>
2024-03-06 11:36:20 +0700

    fresh

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,34 +1,39 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
       <div id="container">
         <strong>Ready to create an app?</strong>
         <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <!-- <ImageBarcodeReader @decode="onDecode" @error="onError"></ImageBarcodeReader> -->
+        <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { ImageBarcodeReader, StreamBarcodeReader } from "vue-barcode-reader";
+import { onMounted, ref } from "vue";
+
+const onDecode = () => {
+    console.log('Decoded successfully');
+}
+
+const onError = (error) => {
+    console.error('Error decoding barcode:', error);
+}
+
+const onLoaded = () => {
+    console.log('Barcode reader loaded');
+}
+onMounted(()=>{
+
+})
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
   position: absolute;
   left: 0;
   right: 0;
@@ -44,9 +49,7 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
   color: #8c8c8c;
-  
   margin: 0;
 }
 
